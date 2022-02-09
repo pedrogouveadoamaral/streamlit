bb4f03e1e8c8aa2e13983b641fe089c4feb82dae
pedrogouveadoamaral <pedrogouveadoamaral@gmail.com>
2022-02-08 21:49:31 -0300

    Adicionei um gráfico de pizza mostrando a proporção de cada variante para o filtro selecionado, mas quando escolhe uma variante, ainda não está 100%.

--- streamlit.py
+++ streamlit.py
@@ -4,10 +4,9 @@ import plotly.express as px
 
 
 df = pd.read_csv('covid-variants.csv')
+
 df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
-
 paises = list(df['location'].unique())
-
 variants = list(df['variant'].unique())
 
 tipo = 'Casos diários'
@@ -15,6 +14,7 @@ titulo = tipo + ' para '
 
 pais = st.sidebar.selectbox('Selecione o pais', ['Todos'] + paises)
 variante = st.sidebar.selectbox('Selecione a variante', ['Todas'] + variants)
+
 
 if (pais != 'Todos'):
     st.header('Mostrando dados para ' + pais)
@@ -36,3 +36,10 @@ dfShow = df.groupby(by=["date"]).sum()
 fig = px.line(dfShow, x=dfShow.index, y='num_sequences')
 fig.update_layout(title=titulo)
 st.plotly_chart(fig, use_container_width=True)
+
+df2 = px.data.tips()
+print(df2)
+fig2 = px.pie(df, names='variant', values='num_sequences')
+fig.update_layout(title=titulo)
+st.plotly_chart(fig2, use_container_width=True)
+
